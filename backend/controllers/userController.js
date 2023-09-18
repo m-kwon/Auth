71ca176921ae78b88e2fe5ebbd51ca1dc6b52b8d
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-17 21:35:41 -0700

    Tested new error handlers which help with readability of errors

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,14 @@
+import asyncHandler from 'express-async-handler';
+
 // @desc   Auth user/set token
 // @route  POST /api/users/auth
 // @access Public
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
+  res.status(401);
+  throw new Error('Something went wrong');
   res.status(200).json({ message: 'Auth User' })
-}
+});
 
 export { authUser };
+
+// mongoose returns promises
