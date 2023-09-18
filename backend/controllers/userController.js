63d5116e6b99ca97a3d7bed481ebe6232b4f9443
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-18 15:43:32 -0700

    Implemented controller for logging out

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -17,14 +17,14 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     name,
     email,
-    password
+    password,
   });
   if (user) {
     generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
-      email: user.email
+      email: user.email,
     });
   } else {
     res.status(400);
@@ -44,7 +44,7 @@ const authUser = asyncHandler(async (req, res) => {
     res.status(201).json({
       _id: user._id,
       name: user.name,
-      email: user.email
+      email: user.email,
     });
   } else {
     res.status(401);
@@ -56,6 +56,11 @@ const authUser = asyncHandler(async (req, res) => {
 // @route  POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
   res.status(200).json({ message: 'Logout User' });
 });
 
