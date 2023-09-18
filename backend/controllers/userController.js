4120495e9839e36e1deb5d17a981e106c8e5a9da
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-17 22:41:43 -0700

    Implemented user creation with a check for users that already exist; still need to hash password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,40 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
 // @desc   Register a new user
 // @route  POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Register User' })
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    res.status(400);
+    throw new Error('User with that e-mail already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // @desc   Auth user/set token
 // @route  POST /api/users/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Auth User' })
+  res.status(200).json({ message: 'Auth User' });
 });
 
 
@@ -19,21 +42,21 @@ const authUser = asyncHandler(async (req, res) => {
 // @route  POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Logout User' })
+  res.status(200).json({ message: 'Logout User' });
 });
 
 // @desc   Get user profile
 // @route  GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'User Profile' })
+  res.status(200).json({ message: 'User Profile' });
 });
 
 // @desc   Update user profile
 // @route  PUT /api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Update User Profile' })
+  res.status(200).json({ message: 'Update User Profile' });
 });
 
 export {
