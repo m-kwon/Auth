518ce11694043e483d87a340d8ac4faa8e05aa8d
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-17 21:54:58 -0700

    Chained get and put requests and consolidated code since they both route to /profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,7 +12,9 @@ const router = express.Router();
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router.get('/profile', getUserProfile);
-router.put('/profile', updateUserProfile);
+router
+  .route('profile')
+  .get(getUserProfile)
+  .put(updateUserProfile);
 
 export default router;
