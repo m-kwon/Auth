787711e7ee0f0ee19dd92118f0c08545d47206f4
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-18 19:46:26 -0700

    Applied React Toastify to better user clarity to see if they messed up their login credentials; also made page indicate if user was logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import { Outlet } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 
 const App = () => {
   return (
     <>
-    <Header />
-    <Container className='my-2'>
-      <Outlet />
-    </Container>
+      <Header />
+      <ToastContainer />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
     </>
   );
 };
