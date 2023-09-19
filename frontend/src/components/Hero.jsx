1803ca4666a6388d6d734d79ae28a0ddfca59efa
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-18 18:10:21 -0700

    Misc

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -6,11 +6,9 @@ const Hero = () => {
     <div className=' py-5'>
       <Container className='d-flex justify-content-center'>
         <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
-          <h1 className='text-center mb-4'>MERN Authentication</h1>
+          <h1 className='text-center mb-4'>Authentication</h1>
           <p className='text-center mb-4'>
-            This is a boilerplate for MERN authentication that stores a JWT in
-            an HTTP-Only cookie. It also uses Redux Toolkit and the React
-            Bootstrap library
+            A full-stack MERN application for user management and authentication using JSON Web Tokens and HttpOnly cookies
           </p>
           <div className='d-flex'>
             <LinkContainer to='/login'>
