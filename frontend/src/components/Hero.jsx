0c3077d20a1c66bd96c0635e85fe52e446d89ba0
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-09-19 23:46:27 -0700

    Added a logout button to the home screen if user is logged in

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,7 +1,26 @@
 import { Container, Card, Button } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { logout } from '../slices/authSlice';
+import { useLogoutMutation } from '../slices/usersApiSlice';
 
 const Hero = () => {
+  const { userInfo } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [logoutApiCall] = useLogoutMutation();
+  const logoutHandler = async () => {
+    try {
+      alert('Signing out');
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className=' py-5'>
       <Container className='d-flex justify-content-center'>
@@ -11,16 +30,28 @@ const Hero = () => {
             A full-stack MERN application for user management and authentication using JSON Web Tokens and HttpOnly cookies
           </p>
           <div className='d-flex'>
-            <LinkContainer to='/login'>
-              <Button variant='primary' className='me-3'>
-                Sign In
-              </Button>
-            </LinkContainer>
-            <LinkContainer to='/register'>
-              <Button variant='secondary'>
-                Register
-              </Button>
-            </LinkContainer>
+            {!userInfo ? (
+              <>
+                <LinkContainer to='/login'>
+                  <Button variant='primary' className='me-3'>
+                    Sign In
+                  </Button>
+                </LinkContainer>
+                <LinkContainer to='/register'>
+                  <Button variant='secondary' className='me-3'>
+                    Register
+                  </Button>
+                </LinkContainer>
+              </>
+            ) : (
+              <>
+                <LinkContainer to='/'>
+                  <Button variant='secondary' onClick={logoutHandler}>
+                    Logout
+                  </Button>
+                </LinkContainer>
+              </>
+            )}
           </div>
         </Card>
       </Container>
